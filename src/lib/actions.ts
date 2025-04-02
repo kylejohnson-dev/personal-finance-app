86406bc421d315a13e3b638e09a3b827ae57b756
kylejohnson-dev <160078497+kylejohnson-dev@users.noreply.github.com>
2025-04-01 20:13:12 -0500

    updated actions file

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,6 +1,7 @@
 import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { z } from "zod";
 
 //Budgets
 export async function createBudget(formData: FormData) {
@@ -17,12 +18,57 @@ export async function deleteBudget(id: string) {
 }
 
 // Pots
-export async function createPot(formData: FormData) {
+const PotFormSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  target: z.coerce.number(),
+  total: z.coerce.number(),
+  theme: z.string(),
 
+});
+
+const CreatePot = PotFormSchema.omit({ id: true });
+const UpdatePot = PotFormSchema.omit({ id: true });
+
+export async function createPot(formData: FormData) {
+  const { name, target, total, theme } = CreatePot.parse({
+    name: formData.get('name'),
+    target: formData.get('target'),
+    total: formData.get('total'),
+    theme: formData.get('theme'),
+  });
+
+  const targetInCents = target * 100;
+  const totalInCents = total * 100;
+
+  await sql`
+    INSERT INTO pots (name, target, total, theme)
+    VALUES (${name}, ${targetInCents}, ${totalInCents}, ${theme})
+  `;
+
+  revalidatePath('/dashboard/pots');
+  redirect('/dashboard/pots');
 }
 
 export async function updatePot(id: string, formData: FormData) {
-  
+  const { name, target, total, theme } = UpdatePot.parse({
+    name: formData.get('name'),
+    target: formData.get('target'),
+    total: formData.get('total'),
+    theme: formData.get('theme'),
+  })
+
+  const targetInCents = target * 100;
+  const totalInCents = total * 100;
+
+  await sql`
+    UPDATE pots
+    SET name = ${name}, target = ${targetInCents}, total = ${totalInCents}, theme = ${theme}
+    WHERE id = ${id}
+  `;
+
+  revalidatePath('/dashboard/pots');
+  redirect('/dashboard/pots');
 }
 
 export async function deletePot(id: string) {
